531a02188a5bda096611174d3b165578bbb43770
Hyago Oliveira <hyagogow@gmail.com>
2023-05-22 11:45:19 -0300

    Add Product Model

---
+++ src/main/java/com/api/springstore/models/Product.java
@@ -0,0 +1,27 @@
+package com.api.springstore.models;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 120)
+    private String name;
+    @Column(length = 300)
+    private String pictureUrl;
+    @Column(length = 1024)
+    private String description;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private LocalDateTime registration;
+}
